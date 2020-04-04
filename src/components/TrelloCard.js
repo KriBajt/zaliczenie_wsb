c601a8609d068acb1d51ddfadaec4f4b1c256a0a
kribajt <kribajt@o2.pl>
2020-04-04 20:41:55 +0200

    dodano materialize do card

--- src/components/TrelloCard.js
+++ src/components/TrelloCard.js
@@ -1,8 +1,21 @@
 import React from "react";
+import Card from '@material-ui/core/Card';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import Button from '@material-ui/core/Button';
+import Typography from '@material-ui/core/Typography';
 
 const TrelloCard = () => {
     return (
-        <h3>Hello card</h3>
+        <>
+            <Card>
+                <Typography
+                    gutterBottom>
+                    Word of the Day
+        </Typography>
+
+            </Card>
+        </>
     )
 }
 
--- src/components/TrelloList.js
+++ src/components/TrelloList.js
@@ -10,14 +10,15 @@ const TrelloList = ({ title }) => {
                 <TrelloCard />
             </div>
         </>
-    )
-}
+    );
+};
 
 const styles = {
     container: {
         backgroundColor: '#ccc',
         borderRadius: 3,
-        width: 300
+        width: 300,
+        padding: 8
     }
 }
 export default TrelloList;
