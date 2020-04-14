f6e1769d617a38c824f2a3c33206e1dc2dc7d7fc
kribajt <kribajt@o2.pl>
2020-04-14 22:25:18 +0200

    edycja kart

--- src/components/TrelloActionButton.js
+++ src/components/TrelloActionButton.js
@@ -1,7 +1,35 @@
 import React from "react";
 import Icon from "@material-ui/core/Icon";
+import Textarea from 'react-textarea-autosize';
+import Card from '@material-ui/core/Card';
+
 
 class TrelloActionButton extends React.Component {
+
+    state = {
+        formOpen: false,
+        text: ""
+    };
+
+
+    openForm = () => {
+        this.setState({
+            formOpen: true
+        })
+    }
+
+    closeForm = e => {
+        this.setState({
+            formOpen: false
+        });
+    };
+
+    handleInputChange = e => {
+        this.setState({
+            text: e.target.value
+        })
+    }
+
 
     renderAddButton = () => {
         const { list } = this.props;
@@ -13,20 +41,56 @@ class TrelloActionButton extends React.Component {
 
 
         return (
-            <div style={{
-                ...styles.openForButtonGroup,
-                opacity: buttonTextOpacity,
-                color: buttonTextColor,
-                backgroundColor: buttonTextBackground
-            }}
+            <div
+                onClick={this.openForm}
+                style={{
+                    ...styles.openForButtonGroup,
+                    opacity: buttonTextOpacity,
+                    color: buttonTextColor,
+                    backgroundColor: buttonTextBackground
+                }
+                }
             >
                 <Icon>add</Icon>
                 <p>{buttonText}</p>
             </div >
         )
     }
+
+
+
+    renderForm = () => {
+        const { list } = this.props;
+
+        const placeholder = list ? "Enter list title..." : "Entera title for this card...";
+
+        const buttonTitle = list ? "Add List" : "Add Card";
+
+        return <div>
+            <Card>
+                <Textarea
+                    placeholder={placeholder}
+                    autoFocus
+                    onBlur={this.closeForm}
+                    value={this.state.text}
+                    onChange={this.handleInputChange}
+                    style={{
+                        resize: "none",
+                        width: "100%",
+                        outline: "None",
+                        border: "none",
+                    }}
+
+
+                />
+
+            </Card>
+        </div>
+
+    };
+
     render() {
-        return this.renderAddButton();
+        return this.state.formOpen ? this.renderForm() : this.renderAddButton();
     }
 }
 
