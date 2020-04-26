f85a1e8190f9ff3679bacea82bfa929d5a6060fc
kribajt <kribajt@o2.pl>
2020-04-26 14:35:58 +0200

    doinstalowany ckeditor

--- src/components/Card/CardDetail.js
+++ src/components/Card/CardDetail.js
@@ -1,7 +1,9 @@
 import React, { Component } from 'react'
 import { Modal, Row, Col, Form } from 'react-bootstrap';
 import { Button, ButtonToolbar } from 'react-bootstrap';
-
+import CKEditor from '@ckeditor/ckeditor5-react';
+import ClassicEditor from '@ckeditor/ckeditor5-build-classic';
+import './CardDetail.css';
 
 export default class CardDetail extends Component {
     constructor(props) {
@@ -12,6 +14,7 @@ export default class CardDetail extends Component {
     render() {
         return (
             <>
+
                 <Modal
                     {...this.props}
                     size="xl"
@@ -25,31 +28,66 @@ export default class CardDetail extends Component {
                     </Modal.Header>
                     <Modal.Body>
                         <div className="container">
-                            <h4>Centered Modal</h4>
-                            <p>
-                                Cras mattis consectetur purus sit amet fermentum. Cras justo odio,
-                                dapibus ac facilisis in, egestas eget quam. Morbi leo risus, porta ac
-                                consectetur ac, vestibulum at eros.
-                            </p>
-                            <p>
-                                Cras mattis consectetur purus sit amet fermentum. Cras justo odio,
-                                dapibus ac facilisis in, egestas eget quam. Morbi leo risus, porta ac
-                                consectetur ac, vestibulum at eros.
-                            </p>
-                            <p>
-                                Cras mattis consectetur purus sit amet fermentum. Cras justo odio,
-                                dapibus ac facilisis in, egestas eget quam. Morbi leo risus, porta ac
-                                consectetur ac, vestibulum at eros.
-                            </p>
-                            <p>
-                                Cras mattis consectetur purus sit amet fermentum. Cras justo odio,
-                                dapibus ac facilisis in, egestas eget quam. Morbi leo risus, porta ac
-                                consectetur ac, vestibulum at eros.
-                            </p>
+                            <Form>
+                                <div className="row">
+                                    <div className="cardDetailNav">
+                                        <p>Dodaj eplik </p>
+                                        <span> | </span>
+                                        <p> Nazwa projektu</p>
+                                    </div>
+                                </div>
+                                <h4>Przykładowy temat zadania do wykonania...</h4>
+
+                                <h6 className="mt-5">Opis</h6>
+                                <div className="cardDetailContent">
+                                    <div className="col-12 col-md-8 ml-0 pl-0">
+                                        <Form.Group id="cardDetailDescription">
+                                            <CKEditor
+                                                editor={ClassicEditor}
+                                                data="<p>Dodaj opis...</p>"
+                                                onInit={editor => {
+                                                    // You can store the "editor" and use when it is needed.
+                                                    console.log('Editor is ready to use!', editor);
+                                                }}
+                                                onChange={(event, editor) => {
+                                                    const data = editor.getData();
+                                                    console.log({ event, editor, data });
+                                                }}
+                                                onBlur={(event, editor) => {
+                                                    console.log('Blur.', editor);
+                                                }}
+                                                onFocus={(event, editor) => {
+                                                    console.log('Focus.', editor);
+                                                }}
+                                            />
+                                        </Form.Group>
+                                    </div>
+                                    <div className="col-md-3">
+                                        <p>dfsdfs</p>
+
+                                    </div>
+
+                                </div>
+
+
+                                <Form.Row>
+                                    <Form.Group as={Col} controlId="formGridZip">
+                                        <Form.Label>Zip</Form.Label>
+                                        <Form.Control />
+                                    </Form.Group>
+                                </Form.Row>
+
+
+
+                                <Button variant="primary" type="submit">
+                                    Kick it!
+                                 </Button>
+                            </Form>
+
                         </div>
                     </Modal.Body>
                     <Modal.Footer>
-                        <Button variant="danger" onClick={this.props.onHide}>Close</Button>
+                        <Button variant="danger" onClick={this.props.onHide}>Kick Out!</Button>
                     </Modal.Footer>
                 </Modal>
             </>
